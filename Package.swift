98d7e301cfabfa931285b572c155b5873319d461
井川司 <tsukasa.ikawa@secual-inc.com>
2017-12-27 18:36:32 +0900

    SwiftKnexをforkしたURLに

--- Package.swift
+++ Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/noppoMan/Prorsum.git", from: "0.3.0")
+        .package(url: "https://github.com/noppoMan/Prorsum.git", from: "0.3.0"),
+//        .package(url: "https://github.com/noppoMan/SwiftKnex.git", from: "0.2.0"),
+        .package(url: "https://github.com/tuken/SwiftKnex.git", .branch("swift4")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Meitnerium2",
-            dependencies: ["Prorsum"]),
+            dependencies: ["SwiftKnex"]),
     ]
 )
