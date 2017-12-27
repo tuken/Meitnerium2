56db69ad39353716c59d62755bcde2014202242f
井川 司 <tsukasa.ikawa@secual-inc.com>
2017-12-28 01:01:39 +0900

    SwiftyJSON、SwiftMsgPack追加

--- Package.swift
+++ Package.swift
@@ -11,12 +11,14 @@ let package = Package(
         .package(url: "https://github.com/noppoMan/Prorsum.git", from: "0.3.0"),
 //        .package(url: "https://github.com/noppoMan/SwiftKnex.git", from: "0.2.0"),
         .package(url: "https://github.com/tuken/SwiftKnex.git", .branch("swift4")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
+        .package(url: "https://github.com/malcommac/SwiftMsgPack.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Meitnerium2",
-            dependencies: ["SwiftKnex"]),
+            dependencies: ["SwiftKnex", "SwiftyJSON", "SwiftMsgPack"]),
     ]
 )
