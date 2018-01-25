569af0ab352008a62d80623f0ca33cb0a92d7dfd
井川司 <tsukasa.ikawa@secual-inc.com>
2018-01-25 19:55:59 +0900

    ProrsumにURandomがあったので、やっぱTurnstileパッケージを削除

--- Package.swift
+++ Package.swift
@@ -13,13 +13,12 @@ let package = Package(
 //        .package(url: "https://github.com/tuken/SwiftKnex.git", .branch("swift4")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
         .package(url: "https://github.com/malcommac/SwiftMsgPack.git", from: "1.0.0"),
-        .package(url: "https://github.com/stormpath/Turnstile.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Meitnerium2",
-            dependencies: ["SwiftKnex", "SwiftyJSON", "SwiftMsgPack", "Turnstile"]),
+            dependencies: ["SwiftKnex", "SwiftyJSON", "SwiftMsgPack"]),
     ]
 )
